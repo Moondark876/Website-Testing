c4a609be0d80efa876d91f4aafe20685dd4b9ed5
Moondark876 <93305914+Moondark876@users.noreply.github.com>
2022-07-02 18:05:21 -0500

    Add files via upload

---
+++ app/compiler.php
@@ -0,0 +1,34 @@
+<?php
+    $language = strtolower($_POST['language']);
+    $code = $_POST['code'];
+
+    $random = substr(md5(mt_rand()), 0, 7);
+    $filePath = "temp/" . $random . "." . $language;
+    $programFile = fopen($filePath, "w");
+    fwrite($programFile, $code);
+    fclose($programFile);
+
+    if($language == "moonglow") {
+        $output = shell_exec("MoonGlow $filePath 2>&1");
+    }
+    if($language == "python") {
+        $output = shell_exec("python $filePath 2>&1");
+        echo $output;
+    }
+    if($language == "node") {
+        rename($filePath, $filePath.".js");
+        $output = shell_exec("node $filePath.js 2>&1");
+        echo $output;
+    }
+    if($language == "c") {
+        $outputExe = $random . ".exe";
+        shell_exec("gcc $filePath -o $outputExe");
+        $output = shell_exec(__DIR__ . "//$outputExe");
+        echo $output;
+    }
+    if($language == "cpp") {
+        $outputExe = $random . ".exe";
+        shell_exec("g++ $filePath -o $outputExe");
+        $output = shell_exec(__DIR__ . "//$outputExe");
+        echo $output;
+    }
